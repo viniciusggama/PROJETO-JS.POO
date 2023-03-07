4ada4e9f3a5f4fca958ea10693bcd22de085ad25
Vinicius Gama Santos <114769520+viniciusggama@users.noreply.github.com>
2023-03-06 22:10:47 -0300

    atualizando pasta de imagem

--- app.js
+++ app.js
@@ -5,13 +5,5 @@ import ingredientes from './ingredientes.js';
 
    
 
-/*  var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
-    var resultadoCapitalizao = modificador.capitalizar(ingredientes);
-    var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizao);
-    var resultadorCaixaAlta = modificador.caixaAlta(ingredientes);
-    
-    console.log(resultadoCapitalizao);
-    console.log(resultadoOrdenacao);
-    console.log(resultadorCaixaAlta);
-*/
+
    
