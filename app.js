31249e6e4328f1d89bf254c316c738dad219ee58
Vinicius Gama Santos <114769520+viniciusggama@users.noreply.github.com>
2023-03-11 15:29:05 -0300

    atualizando app.js

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 import modificador from './modificador.js';
 import ingredientes from './ingredientes.js';
 
+let ingredientesOrdenados = modificador.ordenar(ingredientes,'nome');
+let ingredienteCapitalizados = modificador.capitalizar(ingredientesOrdenados,'nome')
+let containerIngredientes = window.document.getElementById('container-ingredientes')
+
+for(let ingrediente of ingredientesOrdenados){
+   let textoHTML = `
+       <div class="ingrediente">
+             <img src="img/${ingrediente.img}"/>
+             <p class="nome-ingrediente">${ingrediente.nome}</p>
+    </div>
+   `;
+
+containerIngredientes.innerHTML += textoHTML;
+
+}
 
 
-   
+
+
 
 
    
